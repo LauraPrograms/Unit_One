5bb9591598221dfe9f6a2f44d60ea3b98023bd73
Laura <Lhuiefiction@gmail.com>
2018-11-14 18:48:24 -0500

    First turn in for C# Lab1

---
+++ CurrencyConverter/CurrencyConverter/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CurrencyConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                double amountOne, amountTwo, amountThree, amountSum, amountAverage;
+                string input;
+                
+               
+                Console.WriteLine("Welcome to Currency Converter. There seems to be three of you. How much money does the first person have?");
+                input = Console.ReadLine();
+                bool success = Double.TryParse(input, out amountOne);
+
+                Console.WriteLine("How much does the second person have?");
+                input = Console.ReadLine();
+                success = Double.TryParse(input, out amountTwo);
+
+                Console.WriteLine("What about the third person?");
+                input = Console.ReadLine();
+                success = Double.TryParse(input, out amountThree);
+
+                //first part of the deliverable//
+                amountSum = amountOne + amountTwo + amountThree;
+                amountAverage = amountSum / 3;
+                Console.WriteLine("Your average is $" + Math.Round(amountAverage,2));
+
+                double[] array1 = { amountOne, amountTwo, amountThree };
+                Console.WriteLine("Your smallest value is $" + array1.Min());
+                Console.WriteLine("Your largest value is $" + array1.Max());
+
+                //second part of deliverable - exchange rate is based off values of 11-11-2018//
+                //$1 = 113.94 Japanese yen, ￥//
+                //$1 = 9.08 Swedish Krona, kr//
+                //$1 = 33.08 Thai Baht, ฿//
+
+                int JapaneseYen;
+                double SwedishKrona, ThaiBaht;
+                
+                JapaneseYen = (int)(amountSum*113.94);
+                SwedishKrona = amountSum * 9.08;
+                ThaiBaht = amountSum * 33.08;
+
+                Console.WriteLine("Your total sum is converted into the three currencies below:");
+                Console.WriteLine("Japanese currency (Yen) = " + JapaneseYen);
+                Console.WriteLine("Swedish currency (Krona) = " + SwedishKrona);
+                Console.WriteLine("Thai currency (Baht) = " + ThaiBaht);
+
+                Console.ReadKey();
+
+
+            }
+        }
+    }
+    //create a consol app in C# that takes in 3 different dollar amounts. Total up those amounts, then calculate and print the following to the console: average, smallest, and largest amount.//
+    //next format and print the total of the three inputs to look like the proper us format as well as the Japanese, Swedish, and Thai//
+    //only two decimal places are allowed.//
+
+}
+    
+
---
+++ FunRecommendations/FunRecommendations/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunRecommendations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string mood, 
+                inputM,
+                transport ="",
+                goodFood = "To Taco Bell - hahaha no, go to Steak & Shake"; 
+            
+            int inputP;
+
+            Console.WriteLine("Howdy user! What are you in the mood for? (please type in one of the following inputs)");
+            string m = "1. Action" + "\r\n" + "2. Chilling" + "\r\n" + "3. Danger" + "\r\n" + "4. Good Food" + "\r\n" + "5. Celebratory" + "\r\n";
+            Console.Write(m);
+            inputM = Console.ReadLine();
+            mood = inputM.ToLower();
+
+            Console.WriteLine("How many people are you bringing with you? (please enter an integer)");
+            inputP = Convert.ToInt32(Console.ReadLine());
+            if (inputP == 0)
+            {
+                transport = "sneakers.";
+            }
+            else if (inputP >= 1 && inputP <= 4)
+            {
+                transport = "a sedan.";
+            }
+            else if (inputP >= 5 && inputP <= 10)
+            {
+                transport = "a Volkswagen bus.";
+            }
+            else if (inputP >= 11)
+            {
+                transport = "an airplane.";
+            }
+            else
+            {
+                Console.WriteLine("I'm sorry, that is a number I do not know.");
+            }
+
+
+            if (transport != "")
+            {
+                if (mood == "action" || mood == "1" || mood == "one" || mood == "1.")
+                {
+                    Console.WriteLine("You should go Stock Car Racing and travel in  " + transport);
+
+                }
+                else if (mood == "chilling" || mood == "2" || mood == "two" || mood == "2.")
+                {
+                    Console.WriteLine("You should go Hiking and travel in " + transport);
+
+                }
+                else if (mood == "danger" || mood == "3" || mood == "three" || mood == "3.")
+                {
+                    Console.WriteLine("You should go Skydiving and travel in " + transport);
+
+                }
+                else if (mood == "good food" || mood == "4" || mood == "four" || mood == "4.")
+                {
+                    Console.WriteLine("You should go " + goodFood +"and travel in " + transport);
+                    
+                }
+                else if (mood == "celebratory" || mood == "5" || mood == "five" || mood == "5.")
+                {
+                    Console.WriteLine("You should go dancing and travel in " + transport);
+                    
+                }
+
+                else
+                {
+                    Console.WriteLine("I'm sorry, your input was not understood. Try again with the listed ones.");
+
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("I'm sorry, your input was not understood. Please enter an integer.");
+            }
+
+            Console.WriteLine("Have fun, goodbye.");
+
+            Console.ReadKey();
+                         
+        }
+    }
+}
